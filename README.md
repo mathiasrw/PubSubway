88664ca8164b49b7e54515c9a85a79df575795ab
Mathiasrw <mathias@rawu.dk>
2014-03-31 23:21:35 +0200

    commit of version 0.18.0

--- README.md
+++ README.md
@@ -1,4 +1,291 @@
 PubSubway
 =========
 
-Plain pubsub module with a spice of logic to when subscribers get the message. PubSubway provides an underground system (of tunnels) helping you with real world callback hell. Think subway in a trafic jam
+#### Plain pubsub module with a spice of logic to when subscribers get the message.
+
+
+Have you ever had 
+
+1. Code you wanted to only invoke once - but with a message that repeats?
+2. Code you wanted to  only invoke when a range of messages have all been published (in random order)?
+3. Code you wanted to pause invoking at speciffic conditions?
+
+Of cause you have... cause thats what real world code needs. 
+
+And everytime you have dealt with it by making some good counters around your pubsub structure and build nicely structured features to take care of when to invoke and when not to. 
+
+PubSubway take all the counters and the logic out of your code and leave easy to read chuncks of code with a light footprint.
+
+    
+Just take the subway instead of sitting in the trafic jam...
+
+
+---- 
+
+_PubSubway provides an underground system (of tunnels) helping you with real world callback hell. Think subway in a trafic jam._
+
+----
+  
+```javascript
+/*
+  	
+  	* Trivial example * 
+	******************/
+	
+  	var go = require('pubsubway');
+
+  	go.subscribe('foo', function(){
+  		console.log('bar')
+  	});
+
+  	go.publish('foo')
+
+```
+
+
+```javascript
+/*
+  	
+  	* More Usefull examle * 
+	***********************/
+	
+  	var db = require('mysql');
+  	var go = require('pubsubway');
+	
+	var mysql = createConnection(db)
+
+  	go.subscibeAND(['/sql_a/done', '/sql_b/done'], function(){
+  		console.log('Both SQL a + b are done now - Hurray...');
+  	});
+
+	mysql.query('UPDATE product SET stock = 7 WHERE id = 423', go.publishBack('/sql_a/done'))
+	
+	mysql.query('UPDATE product SET stock = 0 WHERE id = 151', go.publishBack('/sql_b/done'))
+	
+
+```
+
+
+To use in regular browser just include file as normal (Yay!) 
+
+```HTML
+    <script src="pubsubway.js" type="text/javascript"></script>
+```
+
+In the browser you use the examples with `pubsubway.*` instead of the `go.*` - and ignore the 'require' line.
+
+
+    
+
+
+# The pubsub pattern
+
+Please remember that the pubsub pattern is best suited to publish messages about what **has happened**.
+Dont fall into the pifall of using it as regular functions. A good idea is to label your topics in past sense.
+
+
+
+ToDo: Tests, documentation and better introduction
+
+
+# Documentation
+
+You include the module with a traditional `var go = require('pubsubway');` or any name you find suitable. In the documentation I chose `go` cause it short and sounds nice. 
+
+Depending on the style of program you are developing I personally prefer either long and javaish style method names or short names that fit into the rest of the code more seamless. Thats why the API comes with a bunch of aliases  
+
+
+
+
+## Publish a message
+
+```javascript
+    go.pub =
+    go.yell =  
+    go.publish = function(
+                            topic /* string */ 
+                            ,  
+                            args /* array */
+                            )
+```
+
+
+## Subscribe to a message
+
+To listen to a single message and invoke code is trivial - but when you want to listen to several messages you are suddenly left with a lot of choises recarding how to handle the logic around it. 
+
+
+
+### Subscribe to any of these messages (OR mode)
+
+```javascript
+ 	go.sub =
+   	go.when =
+   	go.subOR =
+   	go.whenOR =
+    go.subscribe = function(		topic			/* string || array */
+									, 
+									callback 		/* Function */
+									, 
+									subscribeFirst 	/* [bool] */
+									,
+									mode			/* string */
+									) 
+ ```     
+
+**Example** 
+   
+```javascript
+	go.subscribe("/foo/bar", function(a, b, c){ 
+		... 
+	}); 
+        
+```          
+
+### Subscribe to any of these messages (OR mode) and invoke only once
+
+```javascript
+ 	go.sub1 =
+   	go.when1 =
+   	go.subOR1 =
+   	go.whenOR1 =
+    go.subscribeOnce = function(	topic			/* string || array */
+									, 
+									callback 		/* Function */
+									, 
+									subscribeFirst 	/* [bool] */
+									,
+									mode			/* string */
+									) 
+ ```     
+	
+
+## Subscribe to all these messages = only start invoking when all of them have been published (AND mode)
+   
+```javascript  
+  	go.subAND =
+  	go.whenAND =
+  	go.subscribeAND =
+    go.subscribeANDmodeContinues = function(
+                                                topic /* string || array */
+                                                , 
+                                                callback /* Function */
+                                                , 
+                                                subscribeFirst /* [bool] */ 
+											)
+```
+
+  	me.subAND1 =
+  	me.whenAND1 = 
+    me.subscribeANDmodeOnce = function(
+                                                topic           /* string || array */
+                                                , 
+                                                callback        /* Function */
+                                                , 
+                                                subscribeFirst  /* [bool] */ 
+                                            ){ 
+        //ToDo:implement reset
+        me.subscribe(topic, callback, subscribeFirst, 'ANDmodeOnce') 
+    }; 
+
+# Subscribe to messages but only start invoking each time all of them have been published again. 
+   
+       
+ ```javascript 
+  	go.subREWIND =
+	go.whenREWIND =
+	go.subscribeREWIND =
+    go.subscribeANDmodeRewind = function(
+    										topic             /* string || array */ 
+    										, 
+    										callback          /* Function */
+    										, 
+    										subscribeFirst    /* [bool] */ 
+    									) 
+```
+
+# Subscribe to messages but pause and restart with 
+  
+											
+```javascript  
+  	go.whenORBUT = 
+  	go.subORBUT = 
+    go.subscribeORmodeButNotIfButResetWith = function(
+                                                        topic /* string || array */ 
+                                                        ,
+                                                        butNotIf /* string || array */
+                                                        ,
+                                                        resetWith /* string || array */
+                                                        , 
+                                                        callback /* Function */
+                                                        , 
+                                                        subscribeFirst /* [bool] */
+                                                    ) 
+```          
+    
+# Unsubscribe a subscription
+	
+```javascript  
+    go.unsub =
+    go.unsubscribe = function(handle /* Array */){ 
+	       
+        //    var handle = pubsubway.subscribe("/foo", function(){}); 
+        //    pubsubway.unsubscribe(handle); 
+  		
+  		// ToDo: make it possible to resubscribe a handle
+		// ToDo: implement go.subways[uid].handlers check 
+```	
+	
+	
+
+# Log the publications	
+	
+	
+
+
+ ```javascript
+    go.log = function(/* string */ msg, /* integer */ level){ 
+        //  log events -  
+        //  Please overwrite behavior with something like
+        //  $.log = function(msg){alert(msg)} 
+          
+        if(doLog && level<=100){ 
+            console.log('pub/sub: ' + msg); 
+        } 
+    }; 
+```	
+
+
+# Start/stop logging
+
+```javascript      
+	go.doLog = function(/*bool*/ val)
+```    
+
+# Stop all publications
+
+_The big handbrake_
+
+```javascript      
+    go.voidAction = function(/*bool*/ val){ 
+```
+
+
+
+# Wrab topics to act like traditional callback function to handle strings as callbacks
+
+```javascript
+	// Make it convinient to publish string instead of sending function as callback 
+    // Use in your own funktion like:       
+    // 		callback_function = go.pubsubBack(topic /* sting */)
+    // to be able to put a function or a string to publish as your own callback 
+	
+	go.pubsubBack(topic /* sting */)
+
+```
+
+----
+
+Loosely based on jQuery pub/sub plugin by Peter Higgins, expanded in scope. Rewritten blindly. 
+  	
+License: MIT
---
+++ pubsubway.js
@@ -0,0 +1,421 @@
+/*  
+  
+    NodeJS pub/sub module by Mathias Rangel Wulff (github: mathiasrw)
+
+    Loosely based on jQuery pub/sub plugin by Peter Higgins, expanded in scope. Rewritten blindly. 
+    
+    License: MIT
+
+    Version: 0.19.0
+
+    
+    * Simple example * 
+    ******************
+
+    var go = require('./pubsubway');
+
+    go.sub('foo', function(){
+        console.log('bar')
+    });
+
+    go.pub('foo')
+
+    ******************
+
+    To use in regular browser just include file as normal - Yay...
+    (just avoid the 'require' line and use pubsubway.* instead of the go.* in the example)
+
+
+    
+*/
+
+var pubsubway = function() {
+    var me = this;
+    var proxy = {};
+    var doLog = false;
+    var voidAction = false;
+
+
+    me.subways = []
+
+
+
+    me.log = function(
+        msg /* string */ ,
+        level /* integer */
+    ) {
+        //  log events -  
+        //  Please overwrite behavior with something like
+        //  pubsubway.log = function(msg){alert(msg)} 
+
+        if (doLog && level <= 100) {
+            console.log('pub/sub: ' + msg);
+        }
+    };
+
+    me.voidAction = function( /*bool*/ val) {
+        voidAction = val ? true : false;
+    };
+
+    me.doLog = function( /*bool*/ val) {
+        doLog = val ? true : false;
+    };
+
+    me.publish =
+        me.yell =
+        me.pub =
+        function(
+            topic /* string */ ,
+            args /* mix */
+    ) {
+
+        //      pubsubway.publish("/some/topic", ["a","b","c"]); 
+
+        //      todo: ? let args be any amount of parameter and not array...
+
+        if ((topic instanceof Array)) {
+            for (var i = 0; i < topic.length; i++) {
+                me.publish(topic[i], args)
+            };
+        }
+
+        if (voidAction) {
+            return me.log('void: ' + topic, 99)
+        }
+
+        me.log(topic, 100)
+
+        args = args || [];
+
+        args = arrayWrap(args);
+
+
+        return proxy[topic] && this.each(proxy[topic], function() {
+            this.apply(me, args);
+        }) ? true : false;
+    };
+
+    me.subscribeANDmodeRewind =
+        me.whenREWIND =
+        me.subREWIND =
+        function(
+            topic /* string || array */ ,
+            callback /* Function */ ,
+            subscribeFirst /* [bool] */
+    ) {
+
+        me.subscribe(topic, callback, subscribeFirst, 'ANDmodeRewind')
+    };
+
+    me.subscribeANDmodeContinues =
+    me.whenAND =
+        me.subAND = function(
+            topic /* string || array */ ,
+            callback /* Function */ ,
+            subscribeFirst /* [bool] */
+    ) {
+        me.subscribe(topic, callback, subscribeFirst, 'ANDmodeContinues')
+    };
+
+    //tpdp: implement subscribeOnce (OR mode)  
+    //me.when1 = 
+    //me.sub1 =
+
+
+    me.subscribeANDmodeOnce =
+        me.whenAND1 =
+        me.subAND1 =
+        function(
+            topic /* string || array */ ,
+            callback /* Function */ ,
+            subscribeFirst /* [bool] */
+    ) {
+        //ToDo:implement reset
+        me.subscribe(topic, callback, subscribeFirst, 'ANDmodeOnce')
+    };
+
+    me.subscribeORmodeButNotIfButResetWith =
+        me.whenORBUT =
+        me.subORBUT =
+        function(
+            topic /* string || array */ ,
+            butNotIf /* string || array */ ,
+            resetWith /* string || array */ ,
+            callback /* Function */ ,
+            subscribeFirst /* [bool] */
+    ) {
+
+        topic = arrayWrap(topic);
+
+        butNotIf = arrayWrap(butNotIf);
+
+        resetWith = arrayWrap(resetWith);
+
+        var settings = []
+        settings[0] = topic
+        settings[1] = butNotIf
+        settings[2] = resetWith
+
+        me.subscribe(settings, callback, subscribeFirst, 'ORmodeButNotIfButResetWith')
+    };
+
+
+    me.subscribe =
+        me.whenOR =
+        me.subOR =
+        me.when =
+        me.sub =
+        function(
+            topic /* string || array */ ,
+            callback /* Function */ ,
+            subscribeFirst /* [bool] */ ,
+            mode /* [string = 'ANDmodeRewind' || 'ANDmodeContinues' || 'ANDmodeOnce' || 'ORmodeButNotIfButResetWith']*/
+    ) {
+
+
+        // example: 
+        // pubsubway.subscribe("/data/loadet", function(arg1, arg2, ... ){ /* do stuf */ }); 
+        // 
+        // ToDo: make parameters like object notation args
+
+        subscribeFirst = subscribeFirst || false
+
+        mode = mode || null
+
+        /* if mode is set topic must be array */
+        if (null != mode && !(topic instanceof Array)) {
+            topic = [topic]
+        }
+
+
+        /* Assign a specific topic to function for normal mode */
+        if (!(topic instanceof Array)) {
+            if (!proxy[topic]) {
+                proxy[topic] = [];
+            }
+
+            if (subscribeFirst) {
+                proxy[topic].unshift(callback);
+            } else {
+                proxy[topic].push(callback);
+            }
+            return [topic, callback];
+        }
+
+        /* IF just normal OR  */
+        if (null == mode) {
+            me.each(topic, function() {
+                me.subscribe(this, callback, mode, subscribeFirst);
+            });
+            return [topic, callback];
+        }
+
+
+        // Init data for the subways 
+        var uid = topic.join('#') + '#' + Math.random()
+        me.subways[uid] = {}
+        me.subways[uid].mux = []
+        me.subways[uid].dontRun = false;
+        me.subways[uid].handlers = []
+        me.subways[uid].handlers.push(me.subscribe(uid, callback))
+
+
+        for (var i in topic) {
+            me.subways[uid].mux[topic[i]] = 1
+        }
+
+        switch (mode) {
+            case 'ANDmodeRewind':
+                me.each(topic, function(theme) {
+                    var theme = this
+                    var thisUid = uid
+                    var handler = me.subscribe(theme, function() {
+                        me.subways[thisUid].mux[theme] = 0;
+
+                        var total = 0;
+                        for (var ii in topic) {
+                            total += me.subways[thisUid].mux[topic[ii]];
+                        }
+
+                        if (0 == total) {
+                            me.publish(thisUid);
+                            for (var ii in topic) {
+                                me.subways[thisUid].mux[topic[ii]] = 1;
+                            }
+                        }
+                    });
+                    me.subways[thisUid].handlers.push(handler)
+                });
+                break;
+
+
+            case 'ANDmodeContinues':
+                me.each(topic, function(theme) {
+                    var theme = this
+                    var thisUid = uid
+                    var handler = me.subscribe(theme, function() {
+                        me.subways[thisUid].mux[theme] = 0;
+
+                        var total = 0;
+                        for (var ii in topic) {
+                            total += me.subways[thisUid].mux[topic[ii]];
+                        }
+
+                        if (0 == total) {
+                            me.publish(thisUid);
+
+                        }
+                    });
+                    me.subways[thisUid].handlers.push(handler)
+                });
+                break;
+
+
+            case 'ANDmodeOnce':
+                me.each(topic, function(theme) {
+                    var theme = this
+                    var thisUid = uid
+                    var handler = me.subscribe(theme, function() {
+
+                        if (me.subways[thisUid].dontRun) {
+                            return
+                        }
+
+                        me.subways[thisUid].mux[theme] = 0;
+
+                        var total = 0;
+                        for (var ii in topic) {
+                            total += me.subways[thisUid].mux[topic[ii]];
+                        }
+
+                        if (0 == total) {
+                            me.publish(thisUid);
+                            me.subways[thisUid].dontRun = true
+                        }
+
+                        // ToDo ? 
+                        /*for(var ii in me.subways[uid].handlers){ 
+                                me.unsubscribe(me.subways[uid].handlers[ii]); 
+                        }*/
+
+                    });
+                    me.subways[thisUid].handlers.push(handler)
+                });
+                break;
+
+
+            case 'ORmodeButNotIfButResetWith':
+                if (!(
+                    (0 in topic) && (topic[0] instanceof Array) &&
+                    (1 in topic) && (topic[1] instanceof Array) &&
+                    (2 in topic) && (topic[2] instanceof Array)
+                )) {
+                    console.log('Wrong pubsub formatting of ORmodeButNotIfButResetWith')
+                    return false;
+                }
+
+                var publish = topic[0]
+                var butNotIf = topic[1]
+                var resetWith = topic[2]
+
+                me.each(resetWith, function(theme) {
+                    var theme = this
+                    var handler = me.subscribe(theme, function() {
+                        me.subways[uid].dontRun = false;
+                    });
+                    me.subways[uid].handlers.push(handler)
+                });
+
+                me.each(butNotIf, function(theme) {
+                    var theme = this
+                    var handler = me.subscribe(theme, function() {
+                        me.subways[uid].dontRun = true;
+                    });
+                    me.subways[uid].handlers.push(handler)
+                });
+
+
+                me.each(publish, function(theme) {
+                    var theme = this
+                    var handler = me.subscribe(theme, function() {
+                        if (me.subways[uid].dontRun) {
+                            return;
+                        }
+                        me.publish(uid);
+                    });
+                    me.subways[uid].handlers.push(handler)
+                });
+
+
+
+
+                break;
+            default:
+                me.log('Wrong mode setting in pubsubway: ' + mode, 1)
+                return false;
+        }
+
+        return [uid, callback];
+
+    };
+
+    me.unsubscribe =
+        me.unsub =
+        function(
+            handle /* array */
+    ) {
+
+        // var handle = pubsubway.subscribe("/foo", function(){}); 
+        // pubsubway.unsubscribe(handle); 
+
+        // ToDo: make it possible to resubscribe a handle
+        // ToDo: implement me.subways[uid].handlers check 
+
+        var t = handle[0];
+        proxy[t] && me.each(proxy[t], function(idx) {
+            if (this == handle[1]) {
+                proxy[t].splice(idx, 1);
+            }
+        });
+    };
+
+
+    me.each = function(
+        topics /* array */ ,
+        callback /* function */
+    ) {
+        var i = -1
+        var length = topics.length
+        while (++i < length && callback.call(el = topics[i], i, el) !== false);
+    }
+
+    var arrayWrap = function(data) {
+        if (!(data instanceof Array)) {
+            return [data]
+        }
+        return data;
+    }
+
+
+    // Make it convinient to publish string instead of sending function as callback 
+    // Use like:       
+    // callback = pubsubway.pubsubBack(callback)
+    // in our own function to be able to put a function to tall OR a string to publish as your own callback 
+    me.pubsubBack = function(callback) {
+        callback = callback || function() {};
+
+        if (!(callback instanceof Function)) {
+            var topic = callback // ToDo: make sure its not an object that only asign a ref
+            callback = function() {
+                me.publish(topic, arguments)
+            }
+        }
+        return callback;
+    }
+
+
+    return me;
+}()
+
+module = module || {};
+module.exports = pubsubway;

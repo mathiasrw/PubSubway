a260338968501acff7730d50ebc5b7e9315f741e
Mathiasrw <mathias@rawu.dk>
2014-04-07 23:13:42 +0200

    dded test stub

---
+++ test/test.js
@@ -0,0 +1,22 @@
+	var test   = require('utest');
+	var is 		= require('assert');
+	var go    	= require('../pubsubway');
+	var should = require('should');
+
+
+
+
+
+test('Just a stub - sorry...', {
+
+  'TEST': function () {
+	is.strictEqual(1, 1);
+  },
+  
+  
+  'TEST2': function () {
+	(5).should.be(5);
+  }
+
+});
+
